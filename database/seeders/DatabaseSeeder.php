3c3761578cc712831b8b262a061e57c6e80f9b2e
Kreed <rnikitav@icloud.com>
2025-11-05 14:01:17 +0300

    Initial comment

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+        ]);
+    }
+}
